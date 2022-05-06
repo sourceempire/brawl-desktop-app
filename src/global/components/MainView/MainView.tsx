fe345ff2c730efa53b417a7137bc236ccfe57651
unknown <jonasgustafson@hotmail.se>
2022-05-06 21:24:30 +0200

    update failed to be found, trying new release

--- src/global/components/MainView/MainView.tsx
+++ src/global/components/MainView/MainView.tsx
@@ -25,7 +25,6 @@ const MainView = () => {
             <>
               <p>Main View</p>
               <p>Version: {process.env.REACT_APP_VERSION}</p>
-              <p>Fett kul att den uppdaterades</p>
               <button onClick={logout}>Log out</button>
             </>
           }
