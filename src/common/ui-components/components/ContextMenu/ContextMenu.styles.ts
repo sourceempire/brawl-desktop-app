621f17d6f0219fd0f94ab698a2892ffdb7d16460
Jonas Gustafson <jonasgustafson@hotmail.se>
2022-07-13 16:13:18 +0200

    added a context menu

---
+++ src/common/ui-components/components/ContextMenu/ContextMenu.styles.ts
@@ -0,0 +1,43 @@
+import styled, { css } from 'styled-components';
+
+/**
+ * In pixels
+ */
+export type Position = { top?: number; right?: number; bottom?: number; left?: number };
+/**
+ * Unit: pixels
+ *
+ * Left will override right
+ */
+export type ArrowPosition = { left?: number; right?: number };
+
+export const Wrapper = styled.div<{ position: Position; arrowPosition?: ArrowPosition }>`
+  position: absolute;
+  padding: 12px;
+
+  ::before {
+    content: '';
+    position: absolute;
+    top: 0px;
+    height: 8px;
+    width: 8px;
+    transform: translateY(-50%) rotate(45deg);
+
+    ${({ arrowPosition, theme }) => css`
+      background-color: ${theme.colors.secondary};
+      left: ${arrowPosition?.left}px;
+      right: ${arrowPosition?.right}px;
+    `}
+  }
+
+  ${({ position, theme }) => css`
+    top: ${position.top}px;
+    right: ${position.right}px;
+    bottom: ${position.bottom}px;
+    left: ${position.left}px;
+
+    background-color: ${theme.colors.secondary};
+
+    border-radius: ${theme.borderRadius.default};
+  `}
+`;
