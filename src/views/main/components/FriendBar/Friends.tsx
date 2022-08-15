d4a6263adf1a6344197516e6d7e21963e9f2d325
Jonas Gustafson <jonasgustafson@hotmail.se>
2022-08-15 22:56:48 +0200

    removed unused code

--- src/views/main/components/FriendBar/Friends.tsx
+++ src/views/main/components/FriendBar/Friends.tsx
@@ -1,10 +1,7 @@
-import useLoggedInUser from 'api/requests/hooks/useLoggedInUser';
 import { Title } from './FriendBar.styles';
 import { Wrapper } from './Friends.styles';
 
 const Friends = () => {
-  const { user: loggedInUser } = useLoggedInUser();
-
   return (
     <Wrapper>
       <Title>Friends 1/3</Title>
