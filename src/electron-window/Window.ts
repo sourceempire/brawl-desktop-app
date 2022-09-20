0cebbc8b07cf2746e347f5875081f2801a2054dd
Jonas Gustafson <jonasgustafson@hotmail.se>
2022-09-20 13:28:45 +0200

    renamed window to electron-window due to conflicts with the browser window object

---
+++ src/electron-window/Window.ts
@@ -0,0 +1,88 @@
+/* eslint-disable */
+
+let Window = {
+  // dummy window for use outside of electron
+  // version: null,
+  openMainWindow: () => {
+    location.href = '/#/main';
+  },
+  openLoginWindow: () => {
+    location.href = '/#/login';
+  },
+  openAuthWindow: (authUrl: any) => {},
+  closeMainWindow: () => {},
+  closeLoginWindow: () => {},
+  // new: (args: any) => {
+  //   return {
+  //     loadURL: (url: any) => {
+  //       window.open(url, '_blank');
+  //     }
+  //   };
+  // },
+  openInBrowser: (url: any) => window.open(url, '_blank'),
+  // setSize: () => {},
+
+  checkForUpdates: () => {},
+  addUpdateCheckListener: (callback: any) => {},
+  addUpdateAvailableListener: (callback: any) => {},
+  addDownloadProgressListener: (callback: any) => {},
+  addUpdateDownloadedListener: (callback: any) => {},
+  addUpdateNotAvailableListener: (callback: any) => {},
+  addUpdateErrorListener: (callback: any) => {},
+
+  addLoginResultListener: (callback: any) => {},
+  removeLoginResultListener: () => {},
+
+  quitAndInstall: () => {},
+
+  notification: ({
+    title,
+    subtitle,
+    body,
+    silent,
+    icon
+  }: {
+    title?: string;
+    subtitle?: string;
+    body?: string;
+    silent?: string;
+    icon?: string;
+  }) => {
+    console.log({
+      title,
+      subtitle,
+      body,
+      silent,
+      icon
+    });
+  },
+
+  focus: () => {}
+};
+
+if (window.bridge) {
+  // if electron renderer thread
+
+  Window = {
+    openMainWindow: window.bridge.openMainWindow,
+    openLoginWindow: window.bridge.openLoginWindow,
+    openAuthWindow: window.bridge.openAuthWindow,
+    closeMainWindow: window.bridge.closeMainWindow,
+    closeLoginWindow: window.bridge.closeLoginWindow,
+    openInBrowser: window.bridge.openInBrowser,
+    checkForUpdates: window.bridge.checkForUpdates,
+    addUpdateCheckListener: window.bridge.addUpdateCheckListener,
+    addUpdateAvailableListener: window.bridge.addUpdateAvailableListener,
+    addDownloadProgressListener: window.bridge.addDownloadProgressListener,
+    addUpdateDownloadedListener: window.bridge.addUpdateDownloadedListener,
+    addUpdateNotAvailableListener: window.bridge.addUpdateNotAvailableListener,
+    addUpdateErrorListener: window.bridge.addUpdateErrorListener,
+    addLoginResultListener: window.bridge.addLoginResultListener,
+    removeLoginResultListener: window.bridge.removeLoginResultListener,
+    quitAndInstall: window.bridge.quitAndInstall,
+    notification: window.bridge.notification,
+    focus: window.bridge.focus
+  };
+}
+
+export default Window;
