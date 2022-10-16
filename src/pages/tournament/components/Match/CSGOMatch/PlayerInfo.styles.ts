6ecf3ba814c965fe42612e0831699f9cf16ba58a
Jonas Gustafson <jonas@sourceempire.io>
2022-10-16 16:34:53 +0200

    added team leader indication to team (#35)

--- src/pages/tournament/components/Match/CSGOMatch/PlayerInfo.styles.ts
+++ src/pages/tournament/components/Match/CSGOMatch/PlayerInfo.styles.ts
@@ -2,10 +2,17 @@ import styled, { css } from 'styled-components/macro';
 import EllipsisText from 'common/components/EllipsisText';
 import { InnerWrapper } from 'common/components/EllipsisText/EllipsisText.styles';
 import { ProfileImage } from 'frames/main/friends/components/Shared.styles';
+import Icons from 'assets/icons/Icons';
 
-export const PlayerProfileImage = styled(ProfileImage)<{ transparent: boolean }>`
+export const ProfileImageWrapper = styled.div`
+  position: relative;
   height: 69px;
   width: 69px;
+`;
+
+export const PlayerProfileImage = styled(ProfileImage)<{ transparent: boolean }>`
+  height: 100%;
+  width: 100%;
 
   ${({ transparent }) =>
     transparent &&
@@ -13,6 +20,26 @@ export const PlayerProfileImage = styled(ProfileImage)<{ transparent: boolean }>
       opacity: 0.5;
     `}
 `;
+
+export const LeaderIcon = styled(Icons.Star)<{ reversed?: boolean }>`
+  position: absolute;
+  height: 25px;
+  fill: #ffcc00;
+
+  ${({ reversed }) =>
+    reversed
+      ? css`
+          left: 0;
+          bottom: 0;
+          transform: translate(-8px, 4px);
+        `
+      : css`
+          bottom: 0;
+          right: 0;
+          transform: translate(8px, 4px);
+        `}
+`;
+
 export const UserTag = styled(EllipsisText)<{ transparent: boolean }>`
   padding: 20px;
   font-size: 16px;
--- src/pages/tournament/components/Match/CSGOMatch/PlayerInfo.tsx
+++ src/pages/tournament/components/Match/CSGOMatch/PlayerInfo.tsx
@@ -1,21 +1,25 @@
 import { useUserFeed } from 'api/feeds';
 import { UserId } from 'types/user/User';
-import { PlayerProfileImage, UserTag } from './PlayerInfo.styles';
+import { LeaderIcon, PlayerProfileImage, ProfileImageWrapper, UserTag } from './PlayerInfo.styles';
 import tempProfileImage from 'assets/images/temporary-profile-image.jpg';
 
 type Props = {
   userId: UserId;
   transparent: boolean;
+  reversed?: boolean;
 };
 
-const PlayerInfo = ({ userId, transparent }: Props) => {
+const PlayerInfo = ({ userId, transparent, reversed }: Props) => {
   const { user, isLoading } = useUserFeed({ userId });
 
   if (isLoading) return null;
 
   return (
     <>
-      <PlayerProfileImage src={tempProfileImage} size="large" transparent={transparent} />
+      <ProfileImageWrapper>
+        <LeaderIcon reversed={reversed} />
+        <PlayerProfileImage src={tempProfileImage} size="large" transparent={transparent} />
+      </ProfileImageWrapper>
       <UserTag transparent={transparent}>{user.userTag}</UserTag>
     </>
   );
