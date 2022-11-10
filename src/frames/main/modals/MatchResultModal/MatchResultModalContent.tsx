25098a3a735342a17277fe2819893d29b1014762
Jonas Gustafson <jonasgustafson@hotmail.se>
2022-11-10 09:03:39 +0100

    cleanup

--- src/frames/main/modals/MatchResultModal/MatchResultModalContent.tsx
+++ src/frames/main/modals/MatchResultModal/MatchResultModalContent.tsx
@@ -1,20 +1,34 @@
+import { useContext } from 'react';
 import { useMatchResultFeed } from 'api/feeds';
+import { Button } from 'common/components';
+import { MatchResultModalContext } from 'context/MatchResultModalContext';
+import CSGOMatchResult from 'pages/tournament/components/MatchHistory/CSGOMatchResult';
 import { isCSGOMatchResult } from 'types/match/Match';
-import CSGOMatchResultModalContent from './CSGOMatchResultModalContent';
-import { Wrapper } from './MatchResultModalContent.styles';
+import { Buttons, Wrapper } from './MatchResultModalContent.styles';
 
 type Props = {
   matchId: string;
 };
 
+// TODO -> Get tournament info if a tournament match
+// TODO -> Add a header with tournament name and "Defeat" or "Victory"
+
 const MatchResultModalContent = ({ matchId }: Props) => {
   const { matchResult, teams } = useMatchResultFeed(matchId);
+  const { hideModal } = useContext(MatchResultModalContext);
 
-  if (isCSGOMatchResult(matchResult)) {
-    return <CSGOMatchResultModalContent matchResult={matchResult} teams={teams} />;
-  }
+  return (
+    <Wrapper>
+      {isCSGOMatchResult(matchResult) && (
+        <CSGOMatchResult matchResult={matchResult} teams={teams} disableBackgroundFadeIn />
+      )}
 
-  return <Wrapper>Not csgo match result</Wrapper>;
+      <Buttons>
+        <Button onClick={hideModal}>Close</Button>
+        <Button>Go to tournament</Button>
+      </Buttons>
+    </Wrapper>
+  );
 };
 
 export default MatchResultModalContent;
