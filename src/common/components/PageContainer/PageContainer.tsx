9ee2c2888a7e7950cd80869d5c871baf8d6a004c
Emil Ottosson <emil_ottosson@hotmail.com>
2023-04-24 00:54:16 +0200

    Changed export in PageContainer

--- src/common/components/PageContainer/PageContainer.tsx
+++ src/common/components/PageContainer/PageContainer.tsx
@@ -1,10 +1,8 @@
 import styled from 'styled-components';
 
-const PageContainer = styled.div`
+export const PageContainer = styled.div`
   max-width: 1440px;
   height: 100%;
   width: 100%;
   margin: auto;
 `;
-
-export default PageContainer;
