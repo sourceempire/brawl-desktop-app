da783dbb15c622c3a74a1cc0a42e9be3e39beffa
Emil Ottosson <emil_ottosson@hotmail.com>
2023-05-20 01:11:11 +0200

    Removed mock data from featured tournaments feed

--- src/api/feeds/hooks/useFeaturedTourmanentsFeed.ts
+++ src/api/feeds/hooks/useFeaturedTourmanentsFeed.ts
@@ -1,6 +1,4 @@
 import { useFeed } from 'brawl-websocket';
-import Game, { GameName } from 'types/Game';
-import { MatchSettingsTypes } from 'types/MatchSettings';
 import { TournamentHub } from 'types/tournaments/TournamentInfo';
 
 type FeedType = {
@@ -10,42 +8,10 @@ type FeedType = {
 const useFeaturedTourmanentsFeed = () => {
   const { data, loading } = useFeed<FeedType>('tournament.hubs.featured');
 
-  // TODO remove this when server provides everything
-  let tournamentHubsWithSettings = fixMatchSettingType(data.featuredTournamentHubs ?? []);
-
-  if (data.featuredTournamentHubs) {
-    tournamentHubsWithSettings = data.featuredTournamentHubs.map((tournamentHub) => {
-      tournamentHub.image =
-        'https://picsum.photos/600/200?random=' + simepleHash(tournamentHub.name);
-      tournamentHub.currentPrizePool = '200.00';
-      tournamentHub.region = 'europe';
-      return tournamentHub;
-    });
-  }
-
   return {
-    featuredTournamentHubs: tournamentHubsWithSettings,
+    featuredTournamentHubs: data.featuredTournamentHubs || [],
     isLoading: loading
   };
 };
 
 export default useFeaturedTourmanentsFeed;
-
-function fixMatchSettingType(tournamentHubs: TournamentHub[]): TournamentHub[] {
-  return tournamentHubs.map((tournamentHub) => {
-    if (tournamentHub.gameId === Game.CSGO) {
-      tournamentHub.matchSettings.__type = MatchSettingsTypes.CSGO;
-      tournamentHub.gameName = GameName[Game.CSGO];
-    }
-    return tournamentHub;
-  });
-}
-
-function simepleHash(str: string): number {
-  return (
-    str
-      .split('')
-      .map((c) => c.charCodeAt(0))
-      .reduce((acc, c) => acc + c) % 10
-  );
-}
