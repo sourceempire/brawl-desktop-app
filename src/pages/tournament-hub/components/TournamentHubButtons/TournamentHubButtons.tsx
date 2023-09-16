a69cd4b41deaa5c38d58df27f847723b52ee079b
Emil Ottosson <emil_ottosson@hotmail.com>
2023-09-16 23:06:08 +0200

    Added prompt button for leave tournament

--- src/pages/tournament-hub/components/TournamentHubButtons/TournamentHubButtons.tsx
+++ src/pages/tournament-hub/components/TournamentHubButtons/TournamentHubButtons.tsx
@@ -1,20 +1,30 @@
+import { useCallback } from 'react';
 import { Button } from 'common/ui';
 import { ButtonsWrapper, LeftButtons, RightButtons } from './TournamentHubButtons.styles';
 import { useTournamentTeamFeed, useTournamentHubFeed } from 'api/feeds';
 import { isFeedWithTeam } from 'api/feeds/hooks/useTournamentTeamFeed';
 import { useLoggedInUser } from 'common/hooks';
 import { useParams } from 'react-router-dom';
+import { PromptButton } from 'common/ui/PromptButton';
+import { useLeaveTournamentRequest } from 'api/requests/tournament';
 
 type Props = {
   handleOpenModal: (name: string) => void;
+  handleCloseModal: () => void;
   openTeamSettings: () => void;
 };
 
-const TournamentHubButtons = ({ handleOpenModal, openTeamSettings }: Props) => {
+const TournamentHubButtons = ({ handleOpenModal, openTeamSettings, handleCloseModal }: Props) => {
   const { hubId } = useParams() as { hubId: string };
   const user = useLoggedInUser();
   const tournamentTeamFeed = useTournamentTeamFeed(hubId, user.id);
   const { tournamentHub } = useTournamentHubFeed(hubId);
+
+  const onLeaveTournamentComplete = useCallback(() => {
+    handleCloseModal();
+  }, []);
+
+  const { leaveTournament } = useLeaveTournamentRequest({ onComplete: onLeaveTournamentComplete });
 
   // TODO -> This is static, should not be recreated on every render, put above the component.
   const buttons = [
@@ -23,6 +33,14 @@ const TournamentHubButtons = ({ handleOpenModal, openTeamSettings }: Props) => {
     { name: 'rules', text: 'Rules' },
     { name: 'howItWorks', text: 'How it works' }
   ];
+
+  const leaveTournamentFunction = () => {
+    leaveTournament({
+      body: {
+        tournamentHubId: hubId
+      }
+    });
+  };
 
   return (
     <ButtonsWrapper>
@@ -40,9 +58,15 @@ const TournamentHubButtons = ({ handleOpenModal, openTeamSettings }: Props) => {
               Join tournament
             </Button>
           ) : (
-            <Button alert onClick={openTeamSettings}>
-              Leave Tournament
-            </Button>
+            <>
+              <Button onClick={openTeamSettings}>Team Settings</Button>
+              <PromptButton
+                promptText="This will affect all team members, are you sure you want to leave this tournament?"
+                alert
+                onClick={leaveTournamentFunction}>
+                Leave Tournament
+              </PromptButton>
+            </>
           ))}
       </RightButtons>
     </ButtonsWrapper>
