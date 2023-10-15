9b41f3b7f086cccfb981a4bc1541bd40a1db1c90
Emil Ottosson <emil_ottosson@hotmail.com>
2023-10-15 19:40:23 +0200

    Added components to MockMatch

--- src/pages/tournament/components/Match/MockMatch/MockMatch.tsx
+++ src/pages/tournament/components/Match/MockMatch/MockMatch.tsx
@@ -1,6 +1,8 @@
 import { MockGameMatch as MockGameMatchType } from 'types/match/Match';
-import { Wrapper } from '../Match.styles';
+import { MiddleContainer, Wrapper } from '../Match.styles';
 import { useMatchContext } from 'context/MatchContext';
+import OngoingMockGameMatch from './OngoingMockGameMatch';
+import Team from './Team';
 
 const MockMatch = () => {
   const { match, team1, team2 } = useMatchContext<MockGameMatchType>();
@@ -9,7 +11,17 @@ const MockMatch = () => {
   console.log('TEAM 1', team1);
   console.log('TEAM 2', team2);
 
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <Team team={team1} />
+
+      <MiddleContainer>
+        <OngoingMockGameMatch />
+      </MiddleContainer>
+
+      <Team team={team2} reversed />
+    </Wrapper>
+  );
 };
 
 export default MockMatch;
