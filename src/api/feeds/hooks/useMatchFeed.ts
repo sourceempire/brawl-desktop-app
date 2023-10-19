55ed2167a034d47e444b8ccbdd18917b775f956d
Emil Ottosson <emil_ottosson@hotmail.com>
2023-10-19 17:25:04 +0200

    Added a default fallback team to feed

--- src/api/feeds/hooks/useMatchFeed.ts
+++ src/api/feeds/hooks/useMatchFeed.ts
@@ -13,19 +13,27 @@ const useMatchFeed = ({ matchId }: Params) => {
 
   const loggedInUser = useLoggedInUser();
 
+  const defaultTeam = {
+    id: '',
+    name: '',
+    players: [],
+    teamLeaderId: '',
+    score: 0
+  };
+
   let team1 = data.gameMatchInfo?.team1
     ? {
         ...data.gameMatchInfo.team1,
         teamLeaderId: data.internalMatchInfo?.team1?.teamLeaderId
       }
-    : null;
+    : defaultTeam;
 
   let team2 = data.gameMatchInfo?.team2
     ? {
         ...data.gameMatchInfo.team2,
         teamLeaderId: data.internalMatchInfo?.team2?.teamLeaderId
       }
-    : null;
+    : defaultTeam;
 
   if (team2 && team2.players.some((player) => player.userId === loggedInUser.id)) {
     [team1, team2] = [team2, team1];
@@ -33,8 +41,8 @@ const useMatchFeed = ({ matchId }: Params) => {
 
   return {
     gameMatchInfo: data.gameMatchInfo ?? {},
-    team1: team1 ?? {},
-    team2: team2 ?? {},
+    team1,
+    team2,
     isLoading: loading
   };
 };
