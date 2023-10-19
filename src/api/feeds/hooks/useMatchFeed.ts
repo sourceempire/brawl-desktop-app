95ca470454d20018868fca91ff3a6c16f69b0d4a
Emil Ottosson <emil_ottosson@hotmail.com>
2023-10-19 16:29:55 +0200

    Added teamLeaderId to team1 and team2

--- src/api/feeds/hooks/useMatchFeed.ts
+++ src/api/feeds/hooks/useMatchFeed.ts
@@ -7,22 +7,34 @@ type Params = {
 };
 
 const useMatchFeed = ({ matchId }: Params) => {
-  const { data, loading } = useFeed<{ gameMatchInfo: Match }>(`match.${matchId}`);
+  const { data, loading } = useFeed<{ gameMatchInfo: Match; internalMatchInfo: Match }>(
+    `match.${matchId}`
+  );
 
   const loggedInUser = useLoggedInUser();
 
-  let team1 = data.gameMatchInfo?.team1;
-  let team2 = data.gameMatchInfo?.team2;
+  let team1 = data.gameMatchInfo?.team1
+    ? {
+        ...data.gameMatchInfo.team1,
+        teamLeaderId: data.internalMatchInfo?.team1?.teamLeaderId
+      }
+    : null;
+
+  let team2 = data.gameMatchInfo?.team2
+    ? {
+        ...data.gameMatchInfo.team2,
+        teamLeaderId: data.internalMatchInfo?.team2?.teamLeaderId
+      }
+    : null;
 
   if (team2 && team2.players.some((player) => player.userId === loggedInUser.id)) {
-    team1 = data.gameMatchInfo?.team2;
-    team2 = data.gameMatchInfo?.team1;
+    [team1, team2] = [team2, team1];
   }
 
   return {
     gameMatchInfo: data.gameMatchInfo ?? {},
-    team1,
-    team2,
+    team1: team1 ?? {},
+    team2: team2 ?? {},
     isLoading: loading
   };
 };
