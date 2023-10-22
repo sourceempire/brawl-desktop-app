58ad6dde0c7025d7ef66f6ad944b25583e521e87
Emil Ottosson <emil_ottosson@hotmail.com>
2023-10-22 22:03:10 +0200

    Changed import of Icons

--- src/pages/tournament/components/Match/MockMatch/PlayerInfo.styles.ts
+++ src/pages/tournament/components/Match/MockMatch/PlayerInfo.styles.ts
@@ -1,8 +1,9 @@
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
-import { EllipsisText, Icons } from 'common/ui';
+import { EllipsisText } from 'common/ui';
 import { InnerWrapper } from 'common/ui/EllipsisText/EllipsisText.styles';
 import { ProfileImage } from 'frames/main/friends/components/Shared.styles';
+import { Icons } from '@sourceempire/brawl-ui';
 
 export const ProfileImageWrapper = styled.div`
   position: relative;
