9cb28271b76970139605d6ec473800bbb939a1e4
Emil Ottosson <emil_ottosson@hotmail.com>
2023-10-22 21:53:39 +0200

    Updated import

--- src/common/components/MatchResult/MockMatchResult/MockMatchResult.styles.ts
+++ src/common/components/MatchResult/MockMatchResult/MockMatchResult.styles.ts
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { Image } from 'brawl-image';
+import { Image } from '@sourceempire/brawl-image';
 import { css } from '@emotion/react';
 import { hsla } from 'utils/styledUtils';
 
