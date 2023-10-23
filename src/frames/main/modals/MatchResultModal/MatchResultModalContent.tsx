77a4ae536a3895f2754ada92bce4bb64d5492ccc
Emil Ottosson <emil_ottosson@hotmail.com>
2023-10-24 01:34:03 +0200

    Changed styling of MatchResultModal

--- src/frames/main/modals/MatchResultModal/MatchResultModalContent.tsx
+++ src/frames/main/modals/MatchResultModal/MatchResultModalContent.tsx
@@ -4,8 +4,9 @@ import { useMatchStatsFeed } from 'api/feeds/hooks/useMatchStatsFeed';
 import CSGOMatchResult from 'common/components/MatchResult/CSGOMatchResult';
 import { Button } from 'common/ui';
 import { MatchResultModalContext } from 'context/MatchResultModalContext';
-import { isCSGOMatch } from 'types/match/Match';
-import { Buttons, Header, Wrapper } from './MatchResultModalContent.styles';
+import { isCSGOMatch, isMockMatch } from 'types/match/Match';
+import { Buttons, Header, TopWrapper, Wrapper } from './MatchResultModalContent.styles';
+import MockMatchResult from 'common/components/MatchResult/MockMatchResult';
 
 type Props = {
   matchId: string;
@@ -13,23 +14,19 @@ type Props = {
 
 const MatchResultModalContent = ({ matchId }: Props) => {
   const { gameMatchInfo, team1, team2, isLoading: isLoadingMatch } = useMatchFeed({ matchId });
-  const {
-    matchStats,
-    roundWins,
-    isLoading: isLoadingMatchStats,
-    hasMatchStats
-  } = useMatchStatsFeed({ matchId });
+  const { matchStats, roundWins } = useMatchStatsFeed({ matchId });
   const { hideModal } = useContext(MatchResultModalContext);
 
-  if (isLoadingMatch || isLoadingMatchStats) return null;
-
-  if (!hasMatchStats) {
-    return <Wrapper>No match result for this match yet</Wrapper>;
-  }
+  if (isLoadingMatch) return null;
 
   return (
     <Wrapper>
-      <Header>Match result</Header>
+      <TopWrapper>
+        <Header>Match result</Header>
+        <Buttons>
+          <Button onClick={hideModal}>Close</Button>
+        </Buttons>
+      </TopWrapper>
 
       {isCSGOMatch(gameMatchInfo) && (
         <CSGOMatchResult
@@ -42,9 +39,14 @@ const MatchResultModalContent = ({ matchId }: Props) => {
         />
       )}
 
-      <Buttons>
-        <Button onClick={hideModal}>Close</Button>
-      </Buttons>
+      {isMockMatch(gameMatchInfo) && (
+        <MockMatchResult
+          gameMatchInfo={gameMatchInfo}
+          team1={team1}
+          team2={team2}
+          disableBackgroundFadeIn
+        />
+      )}
     </Wrapper>
   );
 };
