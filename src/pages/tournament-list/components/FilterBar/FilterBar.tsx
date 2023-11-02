fc4f0756433e519bf3c6335e672c0fa43a3da0e1
Jonas Gustafson <jonasgustafson@hotmail.se>
2023-11-02 23:55:04 +0100

    made a new component for date range picker

--- src/pages/tournament-list/components/FilterBar/FilterBar.tsx
+++ src/pages/tournament-list/components/FilterBar/FilterBar.tsx
@@ -1,8 +1,9 @@
 import { ActionButton, Input } from 'common/ui';
 import styles from './FilterBar.module.css';
-import { DateRangePicker, Icons } from '@sourceempire/brawl-ui';
+import { Icons } from '@sourceempire/brawl-ui';
 import { ActionButtonSize } from 'common/ui/ActionButton/ActionButton.types';
 import { useTournamentHubListContext } from 'pages/tournament-list/context/TournamentHubListContext';
+import { TournamentDateRangePicker } from '../TournamentDateRangePicker';
 
 export function FilterBar() {
   const { actions, state } = useTournamentHubListContext();
@@ -15,27 +16,23 @@ export function FilterBar() {
 
   return (
     <div className={styles.wrapper}>
-      <div className={styles.left_options}>
+      <div className={styles.options}>
         <Input icon={<Icons.Search />} onChange={onSearch} value={state.searchString} />
-        <DateRangePicker
-          date1={state.startTimeFrom}
-          date2={state.startTimeTo}
-          onDate1Change={actions.setStartTimeFrom}
-          onDate2Change={actions.setStartTimeTo}
-        />
+        <TournamentDateRangePicker />
+      </div>
 
+      <div className={styles.options}>
+        <ActionButton
+          icon={<Icons.Filter />}
+          onClick={openFilterModal}
+          size={ActionButtonSize.LARGE}
+        />
         <ActionButton
           icon={<Icons.Filter />}
           onClick={openFilterModal}
           size={ActionButtonSize.LARGE}
         />
       </div>
-
-      <ActionButton
-        icon={<Icons.Filter />}
-        onClick={openFilterModal}
-        size={ActionButtonSize.LARGE}
-      />
     </div>
   );
 }
