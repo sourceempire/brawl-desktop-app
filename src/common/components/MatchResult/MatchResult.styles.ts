f3fa6e11d9731abd0ac6edd65cbc0cbd5dfb51da
Emil Ottosson <emil_ottosson@hotmail.com>
2023-11-14 22:51:18 +0100

    Removed unused match result styling

--- src/common/components/MatchResult/MatchResult.styles.ts
+++ src/common/components/MatchResult/MatchResult.styles.ts
@@ -1,55 +1,11 @@
 import styled from '@emotion/styled';
-import { css, keyframes } from '@emotion/react';
-import { hsla } from 'utils/styledUtils';
+import { css } from '@emotion/react';
 
 export const Wrapper = styled.div`
   position: relative;
   height: 100%;
   ${({ theme }) => css`
     padding: ${theme.spacing.base * 10}px ${theme.spacing.baseX3}px 0;
-  `}
-`;
-
-type BackdropProps = {
-  imageUrl: string;
-  disableFade?: boolean;
-};
-
-const fadeInKeyframes = keyframes`
-  from {
-    opacity: 0;
-    background-position: 0 -200px;
-  } to {
-    opacity: 1;
-    background-position: 0 -230px;
-  }
-`;
-
-export const Backdrop = styled.div<BackdropProps>`
-  position: absolute;
-  z-index: 0;
-  content: '';
-  inset: 0;
-  background-position: 0 -230px;
-  ${({ theme, imageUrl, disableFade }) => css`
-    background-image: url(${imageUrl});
-    background-size: cover;
-
-    ${!disableFade &&
-    css`
-      animation: ${fadeInKeyframes} 0.6s forwards;
-    `}
-
-    :before {
-      content: '';
-      position: absolute;
-      inset: 0;
-      backdrop-filter: grayscale(1);
-      background-image: linear-gradient(
-        ${hsla(theme.colors.secondary.base, 0.8)},
-        ${theme.colors.secondary.base} 80%
-      );
-    }
   `}
 `;
 
