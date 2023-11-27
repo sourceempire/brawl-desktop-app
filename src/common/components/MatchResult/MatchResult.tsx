0003e0517c5f6ecd79e97b779d4bdb424aa53659
Emil Ottosson <emil_ottosson@hotmail.com>
2023-11-27 21:51:29 +0100

    Removed CSGOMatchResult (#122)
    * Removed CSGOMatchResult
    
    * Removed unused match result styling

--- src/common/components/MatchResult/MatchResult.tsx
+++ src/common/components/MatchResult/MatchResult.tsx
@@ -1,9 +1,6 @@
 import { useMatchFeed, useTournamentHubFeed } from 'api/feeds';
-// import { useMatchStatsFeed } from 'api/feeds/hooks/useMatchStatsFeed';
 import { useParams } from 'react-router-dom';
-// import { isCSGOMatch, isMockMatch } from 'types/match/Match';
 import { isMockMatch } from 'types/match/Match';
-// import CSGOMatchResult from './CSGOMatchResult';
 import { Wrapper } from './MatchResult.styles';
 import MockMatchResult from './MockMatchResult';
 import useTournamentFeed from 'api/feeds/hooks/useTournamentFeed';
@@ -11,7 +8,6 @@ import useTournamentFeed from 'api/feeds/hooks/useTournamentFeed';
 const MatchResult = () => {
   const matchId = useParams().matchId as string;
   const { match, team1, team2, isLoading: isLoadingMatch } = useMatchFeed({ matchId });
-  // const { matchStats, roundWins } = useMatchStatsFeed({ matchId });
 
   const { tournamentId } = useParams() as { tournamentId: string };
   const { tournament } = useTournamentFeed({ tournamentId });
@@ -24,17 +20,6 @@ const MatchResult = () => {
 
   if (isLoadingMatch || isLoadingTournamentHub) return null;
 
-  // if (isCSGOMatch(match)) {
-  //   return (
-  //     <CSGOMatchResult
-  //       matchStats={matchStats}
-  //       roundWins={roundWins}
-  //       match={match}
-  //       team1={team1}
-  //       team2={team2}
-  //     />
-  //   );
-  // }
   if (isMockMatch(match)) {
     return (
       <MockMatchResult match={match} team1={team1} team2={team2} imageId={tournamentHub.imageId} />
